f054b61553b3b3c45b4162f39ab8bc82c42969c7
Brian Blankenship <brianblankenship1978@gmail.com>
2021-10-14 17:37:04 -0400

    code checked by automated shell script validation

--- log.sh
+++ log.sh
@@ -1,5 +1,5 @@
 #=================================
-# ***Logging command ouptut as it is without altering it. (Keep Leading spaces and other such formatting)***
+# ***Logging command output as it is without altering it. (Keep Leading spaces and other such formatting)***
 
 # I want this output:
 # $ free -m
@@ -7,42 +7,76 @@
 # Mem:          16265       11421        4619          17         223        4713
 # Swap:          8196         775        7420
 
-# Appened to a date stamp:
+# Append to a date stamp:
 # 20200916-09:54:21-0400:               total        used        free      shared  buff/cache   available
 # 20200916-09:54:21-0400: Mem:          16265       11421        4619          17         223        4713
 # 20200916-09:54:21-0400: Swap:          8196         775        7420
 
 # Log to STDOUT
-log() { while IFS= read -r line; do printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line"; done; };
+log() {
+  while IFS= read -r line; do
+    printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line"
+  done
+}
 
 # or just output to a file defined in "LOGFILE".
-log2file() { [[ "$LOGFILE" == "" ]] && { LOGFILE=$1 }; while IFS= read -r line; do printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" >> $LOGFILE; done; };
+log2file() {
+  [[ "$LOGFILE" == "" ]] && {
+    LOGFILE=$1
+  }
+  while IFS= read -r line; do
+    printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" >>"$LOGFILE"
+  done
+}
 
 # or STDOUT and a file defined in "LOGFILE".
-logand2file() { [[ "$LOGFILE" == "" ]] && { LOGFILE=$1 }; while IFS= read -r line; do printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" | tee -a "$LOGFILE"; done; };
+logand2file() {
+  [[ "$LOGFILE" == "" ]] && {
+    LOGFILE=$1
+  }
+  while IFS= read -r line; do
+    printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" | tee -a "$LOGFILE"
+  done
+}
 
 # log Example.
-free -m |& log; # The '|&' logs ERRORS and STDOUT. Also handles leading spaces.
-cd nothere |& log; # See ERRORS here.
+# The '|&' logs ERRORS and STDOUT. Also handles leading spaces.
+free -m |& log
+# See ERRORS here.
+cd ./not_there |& log
 
 # log2file Example
-free -m |& log2file /tmp/[CASE_NUMBER].log;
+free -m |& log2file /tmp/CASE_NUMBER.log
 
 # logand2file Example
-free -m |& logand2file /tmp/[CASE_NUMBER].log;
+free -m |& logand2file /tmp/CASE_NUMBER.log
 
 # or just output to a file defined in "LOGFILE".
-LOGFILE=/tmp/[CASE_NUMBER-or-whatever].log;
-log2file() { [[ "$LOGFILE" == "" ]] && { LOGFILE=$1 }; while IFS= read -r line; do printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" >> $LOGFILE; done; };
+LOGFILE=/tmp/CASE_NUMBER-or-whatever.log
+log2file() {
+  [[ "$LOGFILE" == "" ]] && {
+    LOGFILE=$1
+  }
+  while IFS= read -r line; do
+    printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" >>"$LOGFILE"
+  done
+}
 
 # or STDOUT and a file defined in "LOGFILE".
-LOGFILE=/tmp/[CASE_NUMBER-or-whatever].log;
-logand2file() { [[ "$LOGFILE" == "" ]] && { LOGFILE=$1 }; while IFS= read -r line; do printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" | tee -a "$LOGFILE"; done; };
+LOGFILE=/tmp/CASE_NUMBER-or-whatever.log
+logand2file() {
+  [[ "$LOGFILE" == "" ]] && {
+    LOGFILE=$1
+  }
+  while IFS= read -r line; do
+    printf '%s\n' "$(date "+%Y%m%d-%H:%M:%S.%N%z"): $line" | tee -a "$LOGFILE"
+  done
+}
 
 # log2file Example
-free -m |& log2file;
+free -m |& log2file
 
 # logand2file Example
-free -m |& logand2file;
+free -m |& logand2file
 
 # Just make a BASH wrapper and call your script like I called "free -m" with "free -m |& log".
